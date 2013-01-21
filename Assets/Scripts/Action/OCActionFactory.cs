e5da6fcfa1520a3d7118b17bac8a8a72e9b40fbf
Oliver Timberlake Watkins Junior <Nemquae@gmail.com>
2013-01-21 16:52:37 +0800

    otw - Created Stub Classes.

---
+++ Assets/Scripts/Action/OCActionFactory.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// OpenCog Action Factory.  Creates OCActions according to a standard protocol.  
+/// Assigns animations to actions and estimates the time the action will take
+/// depending on certain factors (such as the animation's play time, or the motor's speed).
+/// </summary>
+public class OCActionFactory : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
+
---
+++ Assets/Scripts/Action/OCInputController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// OpenCog Input Controller.  Configures keyboard and mouse input 
+/// to correspond with certain action tags.  Supercedes the FPSInputController
+/// </summary>
+public class OCInputController : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
+
