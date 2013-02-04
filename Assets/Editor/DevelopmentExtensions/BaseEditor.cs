5e6a933c1a8d27b547f54f3643ebdd112a32edeb
Lake Watkins <Nemquae@gmail.com>
2013-02-04 10:39:52 +0800

    otw - Weekend Extensibility Refactoring.

--- Assets/Editor/DevelopmentExtensions/BaseEditor.cs
+++ Assets/Editor/DevelopmentExtensions/BaseEditor.cs
@@ -1,3 +1,4 @@
+
 using UnityEngine;
 using UnityEditor;
 using System.Collections;
@@ -10,9 +11,9 @@ using OpenCog;
 public class OCBaseEditor : Editor
 {
 
-  public static bool tried;
+	public static bool tried;
 
-  public static GameObject tryThisObject;
+	public static GameObject tryThisObject;
 
 }
 
@@ -20,137 +21,167 @@ public class OCEditor< OCType > : OCBaseEditor
   where OCType : MonoBehaviour
 {
 
-  OCType m_Instance;
+	OCType m_Instance;
+	
+	PropertyField[] m_fields;
+	
+	public void OnEnable()
+	{
+		m_Instance = target as OCType;
+		m_fields = ExposeProperties.GetProperties(m_Instance);
+	}
+	
+	public override void OnInspectorGUI()
+	{
+	
+		if(target.GetType() == typeof(OCType) && target.GetType() != typeof(MonoBehaviour))
+		{
+			DisplayInspectorGUI();
+			return;
+		}
+	
+		EditorPrefs.SetBool("Fix", GUILayout.Toggle(EditorPrefs.GetBool("Fix", true), "Fix broken scripts"));
+		if(!EditorPrefs.GetBool("Fix", true))
+		{
+			GUILayout.Label("*** SCRIPT MISSING ***");
+			return;
+		}
+		Initialize();
+		var iterator = this.serializedObject.GetIterator();
+		var first = true;
+		while(iterator.NextVisible(first))
+		{
+			first = false;
+			if(iterator.name == "m_Script" && iterator.objectReferenceValue == null)
+			{
+				if((target as Component) != null && tryThisObject == (target as Component).gameObject)
+				{
+					tried = true;
+				}
+				var script = iterator.Copy();
+				var candidates = scripts.ToList();
+				while(iterator.NextVisible(false) && candidates.Count>0)
+				{
+					candidates = candidates.Where(c => c.properties.ContainsKey(iterator.name)).ToList();
+				}
+				if(candidates.Count == 1)
+				{
+					script.objectReferenceValue = candidates[0].script;
+	
+					serializedObject.ApplyModifiedProperties();
+					serializedObject.UpdateIfDirtyOrScript();
+	
+				}
+				else
+				if(candidates.Count > 0)
+				{
+					foreach(var candidate in candidates)
+					{
+						if(candidate != null && candidate.script != null && GUILayout.Button("Use " + candidate.script.name))
+						{
+							script.objectReferenceValue = candidate.script;
+	
+							serializedObject.ApplyModifiedProperties();
+							serializedObject.UpdateIfDirtyOrScript();
+	
+						}
+					}
+				}
+				else
+				{
+					GUILayout.Label("> No suitable scripts were found");
+				}
+				break;
+			}
+		}
+		DisplayInspectorGUI();
+	 
+	}
+	
+	void DisplayInspectorGUI()
+	{
+		base.OnInspectorGUI();
+	
+		ExposeProperties.Expose(m_fields);
+	}
+	
+	void SerializeAndHidePrivateDataMembers(System.Object obj)
+	{
+		if(obj == null)
+		{
+			return;
+		}
+	
+		List< FieldInfo > fields = new List<FieldInfo>();
+	
+		Type objType = obj.GetType();
+	
+		FieldInfo[] infos = objType.GetFields
+		(
+		  BindingFlags.NonPublic
+		| BindingFlags.Instance
+		);
 
-  PropertyField[] m_fields;
- 
-  public void OnEnable()
-  {
-    m_Instance = target as OCType;
-    m_fields = ExposeProperties.GetProperties( m_Instance );
-  }
- 
-  public override void OnInspectorGUI()
-  {
- 
-    if( target.GetType() == typeof( OCType ) && target.GetType() != typeof( MonoBehaviour ) )
-    {
-      DisplayInspectorGUI();
-      return;
-    }
+		foreach( FieldInfo info in infos )
+		{
 
-    EditorPrefs.SetBool( "Fix", GUILayout.Toggle( EditorPrefs.GetBool( "Fix", true ), "Fix broken scripts" ) );
-    if( !EditorPrefs.GetBool( "Fix", true ) )
-    {
-      GUILayout.Label( "*** SCRIPT MISSING ***" );
-      return;
-    }
-    Initialize();
-    var iterator = this.serializedObject.GetIterator();
-    var first = true;
-    while( iterator.NextVisible(first) )
-    {
-      first = false;
-      if( iterator.name == "m_Script" && iterator.objectReferenceValue == null )
-      {
-        if( ( target as Component ) != null && tryThisObject == ( target as Component ).gameObject )
-        {
-          tried = true;
-        }
-        var script = iterator.Copy();
-        var candidates = scripts.ToList();
-        while( iterator.NextVisible(false) && candidates.Count>0 )
-        {
-          candidates = candidates.Where( c => c.properties.ContainsKey( iterator.name ) ).ToList();
-        }
-        if( candidates.Count == 1 )
-        {
-          script.objectReferenceValue = candidates[ 0 ].script;
- 
-          serializedObject.ApplyModifiedProperties();
-          serializedObject.UpdateIfDirtyOrScript();
+			object[] attributes = info.GetCustomAttributes(true);
 
-        }
-        else
-        if( candidates.Count > 0 )
-        {
-          foreach( var candidate in candidates )
-          {
-            if( candidate != null && candidate.script != null && GUILayout.Button( "Use " + candidate.script.name ) )
-            {
-              script.objectReferenceValue = candidate.script;
+		}
 
-              serializedObject.ApplyModifiedProperties();
-              serializedObject.UpdateIfDirtyOrScript();
+	
+	
+	
+	}
+	
+	class ScannedScript
+	{
+		public Dictionary<string, PropertyInfo> properties;
 
-            }
-          }
-        }
-        else
-        {
-          GUILayout.Label( "> No suitable scripts were found" );
-        }
-        break;
-      }
-    }
-    DisplayInspectorGUI();
- 
-  }
-
-  void DisplayInspectorGUI()
-  {
-    base.OnInspectorGUI();
-
-    ExposeProperties.Expose( m_fields );
-  }
-
-  class ScannedScript
-  {
-    public Dictionary<string, PropertyInfo> properties;
-
-    public int id;
-
-    public MonoScript script;
-  }
-  static List<ScannedScript> scripts;
-
-  static bool _initialized = false;
-
-  bool _localInit;
-
-  void Initialize()
-  {
-    if( !_localInit )
-    {
-      EditorApplication.projectWindowChanged += () => {
-        Repaint();
-      };
-      _localInit = true;
-    }
-    if( _initialized )
-    {
-      return;
-    }
-    _initialized = true;
-   
-    ScanAll(); 
-   
-  }
- 
-  void ScanAll()
-  {
-    scripts = Resources.FindObjectsOfTypeAll( typeof( MonoScript ) )
-     .Cast<MonoScript>()
-     .Where( c => c.hideFlags == 0 )
-     .Where( c => c.GetClass() != null )
-     .Select( c => new ScannedScript { id = c.GetInstanceID(), 
-       script = c,
-       properties = c.GetClass()
-          .GetProperties( BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic )
-         .Where( p => p.CanWrite || ( !p.CanWrite && p.IsDefined( typeof( ExposePropertyAttribute ), false ) ) )
-         .ToDictionary( p => p.Name )
-     } )
-     .ToList();
-  }
+		public int id;
+	
+		public MonoScript script;
+	}
+	
+	static List<ScannedScript> scripts;
+	
+	static bool _initialized = false;
+	
+	bool _localInit;
+	
+	void Initialize()
+	{
+		if(!_localInit)
+		{
+			EditorApplication.projectWindowChanged += () => {
+				Repaint();
+			};
+			_localInit = true;
+		}
+		if(_initialized)
+		{
+			return;
+		}
+		_initialized = true;
+	
+		ScanAll();
+	
+	}
+	
+	void ScanAll()
+	{
+		scripts = Resources.FindObjectsOfTypeAll(typeof(MonoScript))
+	     .Cast<MonoScript>()
+	     .Where(c => c.hideFlags == 0)
+	     .Where(c => c.GetClass() != null)
+	     .Select(c => new ScannedScript { id = c.GetInstanceID(),
+	       script = c,
+	       properties = c.GetClass()
+	          .GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
+	         .Where(p => p.CanWrite || (!p.CanWrite && p.IsDefined(typeof(ExposePropertyAttribute), false)))
+	         .ToDictionary(p => p.Name)
+	     })
+	     .ToList();
+	}
 
 }
--- Assets/Scripts/Action/Test.cs
+++ Assets/Scripts/Action/Test.cs
@@ -1,3 +1,19 @@
+/// Unity3D OpenCog World Embodiment Program
+/// Copyright (C) 2013  Novamente
+///
+/// This program is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU Affero General Public License as
+/// published by the Free Software Foundation, either version 3 of the
+/// License, or (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU Affero General Public License for more details.
+///
+/// You should have received a copy of the GNU Affero General Public License
+/// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 using UnityEngine;
 using System.Collections;
 using ProtoBuf;
@@ -8,146 +24,139 @@ namespace OpenCog
 /// <summary>
 /// The Test.
 /// </summary>
+
 [ProtoContract(ImplicitFields = ImplicitFields.AllPublic)]
 public class Test : MonoBehaviour
 {
 
-  /////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 
-  #region Private Member Data
+#region Private Member Data
 
-  /////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 
-  [HideInInspector]
-  [SerializeField]
-  private int m_ExamplePrivateVar = 0;
+	[HideInInspector]
+	[SerializeField]
+	private int m_ExamplePrivateVar = 0;
 
-  /////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////////
 
-  #endregion
+	#endregion
 
-  /////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////////
 
-  #region Accessors and Mutators
+	#region Accessors and Mutators
 
-  /////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////////
 
-  [ExposeProperty]
-  public int ExamplePublicVar
-  {
-    get
-    {
-      return m_ExamplePrivateVar;
-    }
+	[ExposeProperty]
+	public int ExamplePublicVar
+	{
+		get { return m_ExamplePrivateVar; }
 
-    set
-    {
-      m_ExamplePrivateVar = value;
-    }
-  }
+		set { m_ExamplePrivateVar = value; }
+	}
 
-  /////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////////
 
-  #endregion
+	#endregion
 
-  /////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////////
 
-  #region Public Member Functions
+	#region Public Member Functions
 
-  /////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////////
 
-  /// <summary>
-  /// Called when the script instance is being loaded.
-  /// </summary>
-  void Awake()
-  {
+	/// <summary>
+	/// Called when the script instance is being loaded.
+	/// </summary>
+	void Awake()
+	{
+	}
 
-  }
+	/// <summary>
+	/// Use this for initialization
+	/// </summary>
+	void Start()
+	{
+	}
 
-  /// <summary>
-  /// Use this for initialization
-  /// </summary>
-  void Start()
-  {
-  }
+	/// <summary>
+	/// Update is called once per frame.
+	/// </summary>
+	void Update()
+	{
+	}
 
-  /// <summary>
-  /// Update is called once per frame.
-  /// </summary>
-  void Update()
-  {
-  }
+	/// <summary>
+	/// Called once per frame after all Update calls
+	/// </summary>
+	void LateUpdate()
+	{
+	}
 
-  /// <summary>
-  /// Called once per frame after all Update calls
-  /// </summary>
-  void LateUpdate()
-  {
+	/// <summary>
+	/// Raises the enable event when {Name} is loaded.
+	/// </summary>
+	void OnEnable()
+	{
+		Debug.Log
+	(
+		string.Format
+		(
+			"MonoBehaviour[{0}].OnEnable"
+		, gameObject.name + "\\" + GetType().Name
+		)
+	);
+	}
 
-  }
+	/// <summary>
+	/// Raises the disable event when {Name} goes out of
+	/// scope.
+	/// </summary>
+	void OnDisable()
+	{
+		Debug.Log
+	(
+		string.Format
+		(
+			"MonoBehaviour[{0}].OnDisable"
+		, gameObject.name + "\\" + GetType().Name
+		)
+	);
+	}
 
-  /// <summary>
-  /// Raises the enable event when {Name} is loaded.
-  /// </summary>
-  void OnEnable()
-  {
-    Debug.Log
-    (
-      string.Format
-      (
-        "MonoBehaviour[{0}].OnEnable"
-      , gameObject.name + "\\" + GetType().Name
-      )
-    );
-  }
+	/// <summary>
+	/// Raises the destroy event when {Name} is about to be
+	/// destroyed.
+	/// </summary>
+	void OnDestroy()
+	{
+		Debug.Log
+	(
+		string.Format
+		(
+			"MonoBehaviour[{0}].OnDestroy"
+		, gameObject.name + "\\" + GetType().Name
+		)
+	);
+	}
 
-  /// <summary>
-  /// Raises the disable event when {Name} goes out of
-  /// scope.
-  /// </summary>
-  void OnDisable()
-  {
-    Debug.Log
-    (
-      string.Format
-      (
-        "MonoBehaviour[{0}].OnDisable"
-      , gameObject.name + "\\" + GetType().Name
-      )
-    );
-  }
+	/////////////////////////////////////////////////////////////////////////////
 
-  /// <summary>
-  /// Raises the destroy event when {Name} is about to be
-  /// destroyed.
-  /// </summary>
-  void OnDestroy()
-  {
-    Debug.Log
-    (
-      string.Format
-      (
-        "MonoBehaviour[{0}].OnDestroy"
-      , gameObject.name + "\\" + GetType().Name
-      )
-    );
-  }
+	#endregion
 
-  /////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////////
 
-  #endregion
+	#region Private Member Functions
 
-  /////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////////
 
-  #region Private Member Functions
+	/////////////////////////////////////////////////////////////////////////////
 
-  /////////////////////////////////////////////////////////
+	#endregion
 
-  /////////////////////////////////////////////////////////
-
-  #endregion
-
-  /////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////////
 
 }// class {Name}
 
